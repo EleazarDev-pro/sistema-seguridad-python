13e4069d6e46dcce255c5ee6e5e6912071752233
EleazarDev-pro <cruklerrivas20@gmail.com>
2026-03-25 22:25:13 -0400

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,4 +1,21 @@
 import cv2
 
-print("sistema de seguridad iniciado...")
-print("buscando camara...")
+# Abrir la camara de la canaima
+cap = cv2.VideoCapture(0)
+
+print("iniciando camara de seguridad...")
+
+while true:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+  # Mostrar lo que ve la camara
+  cv2.imshow('sistema de seguridad - EleazarDev-pro',frame)
+
+  # si presiona 'q', el programa se cierra
+  if cv2.waitkey(1) & 0xff == ord('q'):
+      break
+
+cap.release()
+cv2.destroyAllwindows()
